55779b81f4fa07dbd444415570478df7ab09ae3c
Beatrice Kibor <kibor9346@gmail.com>
2025-06-03 14:28:16 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Variables used for multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
 
-// Write your code here
+// Random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Variables used for mod
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4; // 24 % 5 = 4
+
+// Max number
+const max = Math.max(1, 5, 10, 15, 20);
